4713175185be8aa51e32196e8bbe521a72ec0e74
pp-qq <geometrylines@gmail.com>
2021-11-22 22:02:31 +0900

    'master'

--- eclsample/run_cifar10.py
+++ eclsample/run_cifar10.py
@@ -72,10 +72,10 @@ class Kings:
         # "MELCHIOR": [False, False, False, False, False, False, True, True, True, True]
         # "ALPHA": [False, False, False, False, False, True, True, True, True, True],
         # "BETA": [True, True, True, True, True, False, False, False, False, False]
-        "N1": [True, True, True, False, False, False, False, False, False, False],
-        "N2": [False, False, True, True, True, False, False, False, False, False],
-        "N3": [False, False, False, False, False, True, True, True, False, False],
-        "N4": [False, False, False, False, False, False, False, True, True, True]
+        "A": [True, True, True, False, False, False, False, False, False, False],
+        "B": [False, False, True, True, True, False, False, False, False, False],
+        "C": [False, False, False, False, False, True, True, True, False, False],
+        "D": [False, False, False, False, False, False, False, True, True, True]
     }
 
     def __init__(self, name, nodes, algorithm="pdmm", device="cpu",
