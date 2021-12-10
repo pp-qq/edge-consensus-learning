37ac54a794ce5e28ca424e512e65f2fe1507bb60
pp-qq <geometrylines@gmail.com>
2021-12-10 15:03:39 +0900

    'master'

--- eclsample/run_cifar10.py
+++ eclsample/run_cifar10.py
@@ -131,15 +131,15 @@ class Kings:
         self.logger.info('Training start!!')
         criterion = nn.CrossEntropyLoss()
         if self.name == 'A':
-            scheduler = torch.optim.lr_scheduler.StepLR(
-                self.optimizer, step_size=10, gamma=0.95, last_epoch=200)
+            # scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.95, last_epoch=200)
             # scheduler = CosineLRScheduler(self.optimizer, t_initial=100, lr_min=2e-5, warmup_t=10, warmup_lr_init=5e-5, warmup_prefix=True)
-            # scheduler = StepLRScheduler(self.optimizer, decay_t=10, warmup_t=10, warmup_lr_init=1e-4, decay_rate=0.95)
+            scheduler = StepLRScheduler(
+                self.optimizer, decay_t=10, warmup_t=10, warmup_lr_init=1e-4, decay_rate=0.95)
         else:
-            scheduler = torch.optim.lr_scheduler.StepLR(
-                self.optimizer, step_size=10, gamma=0.95, last_epoch=200)
+            # scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.95, last_epoch=200)
             # scheduler = CosineLRScheduler(self.optimizer, t_initial=100, lr_min=2e-5, warmup_t=10, warmup_lr_init=5e-5, warmup_prefix=True)
-            # scheduler = StepLRScheduler(self.optimizer, decay_t=10, warmup_t=10, warmup_lr_init=1e-4, decay_rate=0.95)
+            scheduler = StepLRScheduler(
+                self.optimizer, decay_t=10, warmup_t=10, warmup_lr_init=1e-4, decay_rate=0.95)
 
         for epoch in range(max_epoch):   # loop over the dataset multiple times
             running_loss = 0.0
@@ -169,14 +169,14 @@ class Kings:
 
             end_time = time.time()
 
-            scheduler.step()
+            # scheduler.step()
             '''
             if self.name == "A":
                 scheduler.step()
             else:
                 scheduler.step(epoch+1)
             '''
-            # scheduler.step(epoch+1)
+            scheduler.step(epoch+1)
 
             self.latest_epoch = epoch + 1
             latest_loss = running_loss / epc_cnt
